7017d09efeff8196a1ca9f8b66b65e33886b5841
Yukino-Yui <2546517225@qq.com>
2025-11-21 13:52:29 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,27 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 捕获新增员工异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        String str = ex.getMessage();
+        String msg = null;
+        if (str.contains("Duplicate entry")) {
+            String[] s = str.split(" ");
+            String str1 = s[2];
+            msg = str1 + MessageConstant.ALREADY_EXIST;
+            return Result.error(msg);
+        }
+       else {
+            msg = MessageConstant.UNKNOWN_ERROR;
+            return Result.error(msg);
+        }
     }
 
 }
